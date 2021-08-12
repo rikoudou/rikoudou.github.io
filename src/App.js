60b656b6c99311f4ef57d9ff5d5bf41889aff1ef
Rikoudou Sennin <secourk@gmail.com>
2021-08-12 20:29:54 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -7,21 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
           This site is not in use. To check my portfolio, please click <a href = "https://mozeeshan.com"> here</a>.
         </p>
-=======
-          This site is not in use. To check my portfolio please click <a href = "mozeeshan.com"> here</a>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
->>>>>>> 764358c6041485f945c1a8fb5ad60d6fb65f6231
       </header>
     </div>
   );
