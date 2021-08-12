5811038c0e674ed20ee4970b466357cfa6891124
Rikoudou Sennin <secourk@gmail.com>
2021-08-12 19:56:38 +0300

    deploying to github

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This site is not in use. To check my portfolio please click <a href = "https://mozeeshan.com"> here</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>TESTING LOL</h1>
       </header>
     </div>
   );
